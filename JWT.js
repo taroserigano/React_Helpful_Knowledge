ba41de0f931d59c0d606c69ffc7bc6fb9f30e804
Taro Serigano <taroserigano@gmail.com>
2023-02-07 07:14:34 -0600

    Create JWT.js

---
+++ JWT.js
@@ -0,0 +1,40 @@
+
+BASICS: 
+
+jwt.sign(payload, secret, options(expires))  --> return jwt.sign({userId: this._id}, 'jwtSecret', {expiresIn: '1d'}) 
+
+
+CREATION (Mongo): 
+
+UserSchema.methods.createJWT = function(){
+  return jwt.sign({ userId: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME }) 
+} 
+
+
+
+
+
+
+
+AUTH.js 
+
+const token = authHeader.split(' ')[1] 
+
+try{ 
+  const payload = jwt.verify(token, process.env.JWT_SECRET) 
+  console.log(payload) // ==> {userId: 'frgerf342r2f', iat:31434, exp:42123523} 
+    
+  
+} 
+
+
+
+POSTMAN - 
+  
+const jsonData = pm.response.json() 
+pm.global.set("token",jsonData.token) 
+
+
+
+
+
